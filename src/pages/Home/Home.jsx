ad5eff18640d76aa53c262d25c3e96d9cbf0c9d9
aminequ <amhoume@gmail.com>
2022-01-12 23:00:26 +0100

    added scroll to login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import Header from '../../components/Header'
 import { flowers } from '../../helpers/HomeData'
 import './styles.scss';
 import { Icon } from '@iconify/react';
 import Login from './Login';
+import Slide from 'react-reveal/Slide';
+import {Link} from 'react-scroll';
 
 
 
@@ -11,96 +13,115 @@ function ColumnHeroPictures({idx, src, alt}){
 
     return(
         <div className='grid grid-cols-6'>
+            <Slide bottom>
+
             <div className='col1 mt-24'>
                 {
                     
                     flowers[0].col1.map((src, idex) => {
                         return(
-
+                            
                             <img id={idex} src={src.src} alt={src.alt} className='w-[236px] h-[350px] rounded-xl my-2' /> 
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
             </div>
+            </Slide>
+            <Slide bottom> 
             <div className='col2 mt-72'>
                 {
                     
                     flowers[0].col2.map((src, idex) => {
                         return(
-
+                            
                             <img id={idex} src={src.src} alt={src.alt} className='w-[236px] h-[350px] rounded-xl my-2' /> 
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
             </div>
+            </Slide>
+            <Slide bottom>
+
             <div className='col3 mt-96'>
                 {
                     
                     flowers[0].col3.map((src, idex) => {
                         return(
-
+                            
                             <img id={idex} src={src.src} alt={src.alt} className='w-[236px] h-[350px] rounded-xl my-2' /> 
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
             </div>
+            </Slide>
+            <Slide bottom>
+
             <div className='col4 mt-96'>
                 {
                     
                     flowers[0].col4.map((src, idex) => {
                         return(
-
+                            
                             <img id={idex} src={src.src} alt={src.alt} className='w-[236px] h-[350px] rounded-xl my-2' /> 
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
             </div>
+            </Slide>
+            <Slide bottom>
+
             <div className='col5 mt-72'>
                 {
                     
                     flowers[0].col5.map((src, idex) => {
                         return(
-
+                            
                             <img id={idex} src={src.src} alt={src.alt} className='w-[236px] h-[350px] rounded-xl my-2' /> 
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
             </div>
+                    </Slide>
+             <Slide bottom>
             <div className='col6 mt-24'>
                 {
                     
                     flowers[0].col6.map((src, idex) => {
                         return(
-
+                            
                             <img id={idex} src={src.src} alt={src.alt} className='w-[236px] h-[350px] rounded-xl my-2' /> 
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
             </div>
+            </Slide>       
         </div>
     )
 }
+
 
 function Hero(){
 
     var [heroTheme, setHeroThem] = useState('darkYello');
 
+ 
+
     return(
-        <div className='mt-6 h-full w-[1500px] relative' >
+        <div id='hero' className='mt-6 h-screen w-[1500px] relative' >
             <ColumnHeroPictures />
             <div className='hero-text w-full h-[500px] absolute top-0 bg-transparent flex flex-col items-center'>
             <h1 className='text-6xl'>Get Your Next</h1>
               
                 {
                    heroTheme === 'green' 
-                            ? <h1 className='green text-6xl text-green'>Green thumb idea</h1>
+                            ? <Slide bottom> <h1 className='green text-6xl text-green'>Green thumb idea</h1> </Slide>
                             : heroTheme === 'blue'
-                            ? <h1 className='blue text-6xl text-bleu-100'>New look outfit</h1>
+                            ? <Slide bottom> <h1 className='blue text-6xl text-bleu-100'>New look outfit</h1> </Slide>
                             : heroTheme === 'darkYello'
-                            ? <h1 className='dinner text-6xl text-darkYello'>Weeknight dinner</h1>
+                            ? <Slide bottom> <h1 className='dinner text-6xl text-darkYello'>Weeknight dinner</h1> </Slide>
                             : heroTheme === "darkGreen"
-                            ? <h1 className='home text-6xl text-darkGreen'>Home decor idea</h1>
-                            : <h1 className='green text-6xl text-green'> Green thumb idea </h1>
+                            ? <Slide bottom> <h1 className='home text-6xl text-darkGreen'>Home decor idea</h1> </Slide>
+                            : <Slide bottom> <h1 className='green text-6xl text-green'>Green thumb idea</h1> </Slide>
                 }
 
 
@@ -113,20 +134,33 @@ function Hero(){
                 <button onClick={() => setHeroThem("darkGreen")} className={`h-3 w-3 bg-gray rounded-full ${heroTheme === "darkGreen" ? "bg-darkGreen" : "gray"}`}></button>
 
                 <button onClick={() => setHeroThem("green")} className={`h-3 w-3 bg-gray rounded-full ${heroTheme === "green" ? "bg-green" : "gray"}`}></button>
-
                 </div>
-                <button className='see-more flex items-center justify-center rounded-full h-[54px] w-[54px] bg-darkYello absolute bottom-20'><Icon icon="ep:arrow-down-bold" color="white" width="36"/></button>
+                <Link to='login' smooth={true}>
+                <button className={`see-more flex items-center justify-center rounded-full h-[54px] w-[54px] ${
+                    heroTheme === 'darkYello'
+                    ? 'bg-darkYello'
+                    : heroTheme === "green"
+                    ? 'bg-green'
+                    : heroTheme === "darkGreen"
+                    ? 'bg-darkGreen'
+                    : heroTheme === "blue"
+                    ? 'bg-bleu-100'
+                    : 'bg-darkYello'
+                } absolute bottom-20`} ><Icon icon="ep:arrow-down-bold" color="white" width="36"/></button>
+                </Link>
             </div>
         </div>
     )
 }
 
 function Home() {
+
+
     return (
         <div>
             <Header />
             <Hero />
-            <Login />
+            <Login position="top-[100%]" />
         </div>
     )
 }
--- src/pages/Home/Login.jsx
+++ src/pages/Home/Login.jsx
@@ -1,5 +1,7 @@
-import React, { useRef}  from 'react'
+import React, { useRef, useState}  from 'react'
 import { Icon } from '@iconify/react';
+import {Link} from 'react-scroll';
+
 
 function Form(){
 
@@ -44,10 +46,15 @@ function Form(){
 }
 
 
-function Login() {
+
+function Login({position}) {
+
+    var [heroTheme, setHeroThem] = useState('darkYello');
+
+
     return (
-        <div className='h-screen w-screen bg-black bg-black/[.7] absolute top-0 '>
-            <div className='h-screen w-[500px] bg-white rounded-3xl mx-auto relative flex flex-col'>
+        <div id='login' className={`login h-screen w-screen bg-black flex flex-row-reverse justify-between bg-black/[.7] absolute ${position} `}>
+            <div className='h-screen w-[50%] bg-white rounded-3xl  relative flex flex-col'>
                     <button className='rounded-full bg-transparent hover:bg-gray h-[40px] w-[40px] flex justify-center items-center absolute right-3 top-3'><Icon icon="bi:x" width="38" /></button>
                 <div className='w-[40px] h-[40px] mt-6 mx-auto'>
                     <img className='w-[100%] h-[100%]' src="/logo.svg" alt="pinterest logo" />
@@ -56,13 +63,34 @@ function Login() {
                 <Form />
                 <a href='/forgetpassword' className='forgetpassword text-black text-start w-[60%] mx-auto hover:decoration-solid decoration-black	'>Forgot your password?</a>
                 <button className='w-[60%] text-center mx-auto rounded-2xl py-2 mt-6 text-white bg-vermilion-100 hover:bg-vermilion-200'>Log in</button>
-                <p className='text-center text-xl my-2 text-[#333333]'>OR</p>
-                <button className='facebooklogin w-[60%] text-center mx-auto rounded-full py-1 px-4 mt-2 text-xl text-white bg-bleu-100 flex justify-between items-center'><Icon icon="bx:bxl-facebook-circle" color="white" width="32" /> Continue with Facebook</button>
-                <button className='googlelogin w-[60%] text-center mx-auto rounded-full py-1 px-4 text-lg  mt-6 text-[#333333] border-2 border-gray bg-transparent flex justify-between items-center'><Icon icon="flat-color-icons:google" color="white" width="32" />Continue with Google</button>
+                <p className='text-center text-[16px] my-2 text-[#333333]'>OR</p>
+                <button className='facebooklogin w-[60%] text-center mx-auto rounded-full py-1 px-4 text-xl text-white bg-bleu-100 flex justify-between items-center'><Icon icon="bx:bxl-facebook-circle" color="white" width="32" /> Continue with Facebook</button>
+                <button className='googlelogin w-[60%] text-center mx-auto rounded-full py-1 px-4 text-lg  mt-2 text-[#333333] border-2 border-gray bg-transparent flex justify-between items-center'><Icon icon="flat-color-icons:google" color="white" width="32" />Continue with Google</button>
                 <p className='termsservices w-[80%] mx-auto text-center text-[12px] mt-2'>
                 By continuing, you agree to Pinterest's <strong>Terms of Service </strong> and acknowledge you've read our <strong>Privacy Policy </strong>
                 </p>
-
+                <span className='w-[40%] mx-auto bg-gray mt-1 rounded-full h-[0.5px]'></span>
+                <a href="/signup" className='w-[60%] mx-auto text-center text-[12px] mt-1 text-[#333333] cursor-pointer'>Not on Pinterest yet? Sign up</a>
+                <p className='w-[60%] mx-auto text-center text-[12px] mt-1 text-black cursor-pointer'>Are you a business? Get started here!</p>
+                
+            </div>
+            <div className='w-[20%] relative'>
+            <Link to='hero' smooth={true}>
+                <button className={`see-more flex items-center justify-center rounded-full h-[54px] w-[54px] absolute top-[16px] ${
+                    heroTheme === 'darkYello'
+                    ? 'bg-darkYello'
+                    : heroTheme === "green"
+                    ? 'bg-green'
+                    : heroTheme === "darkGreen"
+                    ? 'bg-darkGreen'
+                    : heroTheme === "blue"
+                    ? 'bg-bleu-100'
+                    : 'bg-darkYello'
+                } absolute bottom-20`} ><Icon icon="ep:arrow-down-bold" color="white" width="36"/></button>
+                </Link>
+            </div>
+            <div>
+                <h1 className='text-7xl text-white text-center'>Sign up to access more ideas</h1>
             </div>
         </div>
     )
